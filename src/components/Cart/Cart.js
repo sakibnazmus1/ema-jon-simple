e73c08a1c39c8488377e780f3ee846c0e5c0912e
sakibnazmus1 <sakibnazmus438@gmail.com>
2022-09-27 10:09:03 +0600

    total cart added

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -5,6 +5,10 @@ const Cart = ({ cart }) => {
         <div className='cart'>
             <h4>Order Summary</h4>
             <p>Selected Items: {cart.length}</p>
+            <p>Total Price: </p>
+            <p>Total Shipping: </p>
+            <p>Tax: </p>
+            <h5>Grand Total: </h5>
         </div>
     );
 };
