50861cb6b4953834bb3bdd49a3a5cc1b6d472ad9
sakibnazmus1 <sakibnazmus438@gmail.com>
2022-09-27 10:01:13 +0600

    cast css added

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -1,8 +1,8 @@
 import React from 'react';
-
+import './Cart.css'
 const Cart = ({ cart }) => {
     return (
-        <div>
+        <div className='cart'>
             <h4>Order Summary</h4>
             <p>Selected Items: {cart.length}</p>
         </div>
